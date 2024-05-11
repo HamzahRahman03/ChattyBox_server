899d59db38a97799c9e81a68c918d58682623af7
HamzahRahman03 <112624937+HamzahRahman03@users.noreply.github.com>
2024-05-11 13:38:10 +0530

    dbupdate

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,16 @@ const mongoose = require('mongoose');
 
 const url = "mongodb+srv://HamzahRahman03:Abcde%4012345@cluster0.z4t3ned.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-mongoose.connect(url, {
-    useNewUrlParser: true, 
-    useUnifiedTopology: true
-}).then(() => console.log('Connected to DB')).catch((e)=> console.log('Error', e))
+const connectToDB = async () => {
+    try {
+        await mongoose.connect(url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('Connected to DB');
+    } catch (error) {
+        console.error('Error', error);
+    }
+};
+
+connectToDB();
